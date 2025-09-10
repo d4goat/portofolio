37944e11f65768f27c7884232ec6233a22a61b3f
d4goat <kenzoalgundul@gmail.com>
2025-09-10 22:29:47 +0700

    feat: typewritter text

---
+++ app/components/TypewriterEffect.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="inline-block">
+    {{ displayText }}
+    <span class="animate-pulse">|</span>
+  </span>
+</template>
+
+<script lang="ts" setup>
+const displayText = ref<string>("")
+const props = defineProps<{
+  text: string
+}>()
+
+onMounted(() => {
+  let index = 0;
+  const timer = setInterval(() => {
+    if(index <= props.text.length){
+      displayText.value = props.text.slice(0, index);
+      index++;
+    } else {
+      clearInterval(timer)
+    }
+  }, 200)
+  return () => clearInterval(timer)
+})
+</script>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Suffaq"
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
